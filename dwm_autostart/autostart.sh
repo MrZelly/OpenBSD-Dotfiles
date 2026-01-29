1c3a0b40ed8ee983366e3db2a25da205a9bdaf49
MrZelly <jurko.minaric@gmail.com>
2026-01-29 12:16:32 +0100

    Added .* files

---
+++ dwm_autostart/autostart.sh
@@ -0,0 +1,5 @@
+#feh --bg-center /home/user/Pictures/1.jpg
+/home/user/bg.sh &
+# setxkbmap sk
+slstatus &
+
---
+++ kshrc
@@ -0,0 +1,4 @@
+export PS1='[\u@\h \w]$ '
+
+alias btop='btop --force-utf'
+alias ls='ls -a'
